c49d04bf3d20240a74a35456b3ba0ed50c81b879
YiPay <toono.umi@gmail.com>
2020-05-01 00:29:57 -0400

    add sjango

---
+++ stock_ana/api/models.py
@@ -0,0 +1,14 @@
+from django.db import models
+
+# Create your models here.
+'''
+class Companies(models.Model):
+    Symbol=models.CharField(max_length=10)
+    Name=models.CharField(max_length=200)
+    LastSale=models.FloatField()
+    MarketCap=models.CharField(max_length=200)
+    IPOyear=models.CharField(max_length=200)
+    Sector=models.CharField(max_length=200)
+    industry=models.CharField(max_length=200)
+    Summary_Quote=models.CharField(max_length=200)
+    '''
